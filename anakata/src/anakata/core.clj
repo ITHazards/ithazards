814be5bdde421ef7b8dffa292b68e41e4d3caf34
Ignasi Fosch <natx@y10k.ws>
2017-03-07 00:26:50 +0100

    Adds paginable GET for all vulnerabilities

--- anakata/src/anakata/core.clj
+++ anakata/src/anakata/core.clj
@@ -15,33 +15,39 @@
    :summary s/Str
    :affected-software [s/Str]})
 
-(defonce vulnerabilities (atom []))
+(defn ensure-vulnerability [vulnerability]
+  (if (not (contains? vulnerability :affected-software))
+    (assoc vulnerability :affected-software [])
+    vulnerability))
 
-(defn add! [new-vulnerability]
-  (swap! vulnerabilities conj new-vulnerability))
+(defn read-vulnerabilities [hits]
+  (let [hit (first hits)]
+    (if hit
+      (do
+        (conj
+          (read-vulnerabilities (rest hits))
+          (ensure-vulnerability (get hit :_source))))
+      [])))
 
-(defn get-vulnerabilities []
+(defn get-vulnerabilities [from size]
   (let [conn (esr/connect "http://127.0.0.1:9200")
-        res (esd/search conn "vulnerabilities" "vulnerability")
+        res (esd/search conn "vulnerabilities" "vulnerability"
+                        :from from
+                        :size size)
         hits (esrsp/hits-from res)]
-    (loop [results hits]
-      (let [hit (first results)]
-        (if hit
-          (do
-            (add! (get hit :_source))
-            (recur (rest results))))))
-    (-> vulnerabilities deref)))
+      (read-vulnerabilities hits)))
 
 (def ithazards
   (api
-   {:swagger
-    {:ui "/"
-     :spec "/swagger.json"
-     :data {:info {:title "IT Hazards"}
-            :tags [{:name "api"}]}}}
-   (context "/vulnerabilities" []
-            :tags ["vulnerabilities"]
-            (GET "/" []
-                 :return [Vulnerability]
-                 :summary "Gets vulnerabilities"
-                 (ok (get-vulnerabilities))))))
+    {:swagger
+      {:ui "/"
+       :spec "/swagger.json"
+       :data {:info {:title "IT Hazards"}
+              :tags [{:name "api"}]}}}
+    (context "/vulnerabilities" []
+      :tags ["vulnerabilities"]
+      (GET "/" []
+        :return [Vulnerability]
+        :header-params [{from :- s/Int 0} {size :- s/Int 10}]
+        :summary "Gets vulnerabilities"
+        (ok (get-vulnerabilities from size))))))
